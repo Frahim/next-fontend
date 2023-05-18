77a800b39661c3f8bbbe31ac832543f54e07e115
Md Yeasir Arafat <frahim5@gmail.com>
2023-05-18 19:24:24 +0600

    update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 import HomeBanner from "../components/HomeBanner";
 import SisterConcern from "../components/sisterConcern";
 import Carousel from "../components/carousel";
-import Fact from '../components/FactFiger';
+import Fact from '../components/Factfiger';
 
 
 export default function Home() {
